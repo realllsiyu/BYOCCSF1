44b69a3aa3118a3d79c0a5c0cae0245036fdc3ff
realllsiyu <siyuc0712@gmail.com>
2022-05-17 10:31:39 -0400

    set up

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 const express = require("express");
 const app = express();
+const path = require("path");
 const port = 3000;
+
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "pug");
 
 app.get("/", (req, res) => {
   res.render("index");
